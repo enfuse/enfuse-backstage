4c979893640fc621b9a9640762834c3ca0f554d4
ch-enfuse <chr@enfuse.io>
2022-11-16 13:51:10 -0800

    Removed reference to old plugin.
    Removed App.tsx dangling refernce from old plugin

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -35,7 +35,6 @@ import { PermissionedRoute } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
 import { githubAuthApiRef } from '@backstage/core-plugin-api';
 import { SignInPage } from '@backstage/core-components';
-import { AzureBuildpacksPage } from '@internal/plugin-azure-buildpacks';
 
 const app = createApp({
   apis,
@@ -108,7 +107,6 @@ const routes = (
     </Route>
     <Route path="/settings" element={<UserSettingsPage />} />
     <Route path="/catalog-graph" element={<CatalogGraphPage />} />
-    <Route path="/azure-buildpacks" element={<AzureBuildpacksPage />} />
   </FlatRoutes>
 );
 
